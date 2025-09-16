68c9ac1f5185797e3fc8e5fe0db1939f98dd8192
TCC_Grupo_04 <marianaetecprojetos@gmail.com>
2025-09-16 02:18:07 -0300

    Add files via upload

--- home.php
+++ home.php
@@ -4,19 +4,57 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="home.css">
     <title>Home</title>
 
 </head>
 
 <body>
     <h1>Relp!</h1>
-    <h2>Streamline</h2>
-    <div class="box">
-        <a href="login.php">Acesso Empresa (CEO/Funcionário)</a>
-        <a href="login_fornecedor.php">Acesso Fornecedor</a>
-        <a href="formulario.php">Cadastre sua Empresa</a>
-    </div>
-</body>
+<div class="box box-login">
+  <a href="login.php">Login</a>
+</div>
 
+<div class="box box-fornecedor">
+  <a href="login_fornecedor.php">Acesso Fornecedor</a>
+</div>
+
+<div class="box box-cadastro">
+  <a href="formulario.php">Cadastro</a>
+</div>
+
+<h3>RELP!<br>
+FOR YOUR COMPANY</h3>
+
+<div class="Quem">
+    <!-- Botão que leva até a div "info" -->
+    <a href="#info" class="botaoQ">Quem somos</a>
+  </div>
+
+  <div id="info" class="conteudo">
+    <h2>Quem somos?</h2>
+    <p>Como a Streamline pode te ajudar via RELP!</p>
+  </div>
+
+  <div class="Servicos">
+    <!-- Botão que leva até a div "info" -->
+    <a href="#info2" class="botaoS">Serviços</a>
+  </div>
+
+  <div id="info2" class="conteudo2">
+    <h2>xereca gorda</h2>
+    <p>eu gostum</p>
+  </div>
+<div class="FAQ">
+    <!-- Botão que leva até a div "info" -->
+    <a href="#info3" class="botaoF">FAQ</a>
+  </div>
+
+  <div id="info3" class="conteudo3">
+    <h2>penis</h2>
+    <p>obeso</p>
+  </div>
+
+
+</body>
 </html>
